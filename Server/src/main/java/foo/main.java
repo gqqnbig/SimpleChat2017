cc12c60c45502d514b21e2dd56bc48cfa5964a50
gqqnbig <gqqnb2005@gmail.com>
2017-08-06 14:44:39 -0700

    添加服务器模块
    但运行前似乎要手工删除build目录，需要将来改进。

---
+++ Server/src/main/java/foo/main.java
@@ -0,0 +1,12 @@
+package foo;
+
+/**
+ * Created by loveright on 2017/8/5.
+ */
+
+public class main {
+    public static void main(String[] arg) {
+        String hi = "Hello World";
+        System.out.println(hi);
+    }
+}
