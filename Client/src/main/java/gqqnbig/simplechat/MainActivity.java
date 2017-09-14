6249747b057164082d4ea9a8a252847cfa062fb0
gqqnbig <gqqnb2005@gmail.com>
2017-09-14 10:20:32 -0700

    1

--- Client/src/main/java/gqqnbig/simplechat/MainActivity.java
+++ Client/src/main/java/gqqnbig/simplechat/MainActivity.java
@@ -37,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main1);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         ipText= (EditText)findViewById(R.id.IPTextBox);
